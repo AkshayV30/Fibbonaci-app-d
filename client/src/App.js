fa1081692663b4a278ff6e7968ef429d27d4402f
Akshay <akshayatwork.v30@gmail.com>
2023-01-27 23:32:59 +0530

    update-2.1.3

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
+import React from 'react';
+// import logo from './logo.svg';
 import './App.css';
-import "./App.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import OtherPage from "./OtherPage";
 import Fib from "./Fib";
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/* <img src={logo} className="App-logo" alt="logo" />
           <p></p>
           <a
             className="App-link"
@@ -20,8 +20,8 @@ function App() {
             rel="noopener noreferrer"
           >
            To Learn React
-          </a>
-          <Link to="/">Home</Link>
+          </a> */}
+          {/* <Link to="/">Home</Link> */}
           <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
