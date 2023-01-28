df46b0a16db02ab5ca39930d092152cb78009468
Akshay <akshayatwork.v30@gmail.com>
2023-01-28 13:52:02 +0530

    terra-update-1

--- fib-app/src/App.js
+++ fib-app/src/App.js
@@ -9,8 +9,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        { <img src={logo} className="App-logo" alt="logo" />
+        /*<p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
